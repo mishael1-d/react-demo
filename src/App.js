bae64546aaac313085b5ad8e66ed793ff5b25ca1
Mishael Dada <mishaeldada@gmail.com>
2022-12-16 10:15:13 +0100

    react-chart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,173 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// import { Pie } from "react-chartjs-2";
+import "chart.js/auto";
+import "chartjs-plugin-datalabels";
+import { Chart } from "chart.js";
+import { Pie } from "react-chartjs-2";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+import { Data, NewData, NewData2 } from "./data";
+import { useEffect, useState } from "react";
+
+Chart.register(ChartDataLabels);
 
 function App() {
+  const [label, setLabel] = useState([]);
+  const [dataset, setDataset] = useState([]);
+
+  const [label1, setLabel1] = useState([]);
+  const [dataset1, setDataset1] = useState([]);
+  // useEffect(() => {
+  //   const lab = [];
+  //   const data = [];
+  //   for (let index = 0; index < NewData.length; index++) {
+  //     const element = NewData[index];
+  //     // console.log(Object.keys(element), Object.values(element));
+  //     lab.push(Object.keys(element));
+  //     data.push(JSON.stringify(Object.values(element)));
+  //     // setLabel(lab);
+  //     // setDataset(data);
+
+  //     console.log("Lab: " + lab);
+  //     console.log("data: " + data);
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    const sSubject = Array(NewData2.length).fill(Array(2));
+    const sMarks = Array(NewData2.length).fill(Array(2));
+    // console.log(sSubject);
+    // console.log(sSubject[1]);
+    // console.log(sSubject[2]);
+    for (let index = 0; index < NewData2.length; index++) {
+      // const getData1 = () => {
+      var responses = NewData2[index];
+      // console.log(sSubject[index]);
+      for (let i = 0; i < responses.length; i++) {
+        var eachResponse = responses[i];
+        console.log(eachResponse);
+        // console.log(sSubject[index]);
+
+        sSubject[index] = eachResponse;
+        // sMarks[index] = eachResponse;
+        // console.log("sSubject ith index", sSubject[i]);
+        // console.log(sSubject[index]);
+      }
+
+      console.log(sSubject[index]);
+      // console.log(sMarks);
+
+      setLabel1(sSubject);
+      setDataset1(sMarks);
+    }
+
+    // getData1();
+    // }
+    //console.log(resData);
+  }, []);
+
+  useEffect(() => {
+    const sSubject = [];
+    const sMarks = [];
+    const getData = () => {
+      for (let i = 0; i < NewData.length; i++) {
+        sSubject.push(NewData[i].label);
+        sMarks.push(parseInt(NewData[i].response));
+      }
+      // console.log(sSubject);
+      setLabel(sSubject);
+      setDataset(sMarks);
+    };
+
+    getData();
+    //console.log(resData);
+  }, []);
+
+  const data = {
+    labels: label.map((d) => d),
+    datasets: [
+      {
+        label: "Audit responses",
+        data: dataset.map((d) => d),
+        backgroundColor: ["#4b77a9", "#5f255f"],
+        hoverOffset: 4,
+      },
+    ],
+  };
+
+  const data2 = {
+    labels: label1.map((d) => d),
+    datasets: [
+      {
+        label: "Audit responses",
+        data: dataset1.map((d) => d),
+        backgroundColor: ["#4b77a9", "#5f255f"],
+        hoverOffset: 4,
+      },
+    ],
+  };
+
+  var options = {
+    tooltips: {
+      enabled: false,
+    },
+    plugins: {
+      datalabels: {
+        formatter: (value, ctx) => {
+          let sum = 0;
+          let dataArr = ctx.chart.data.datasets[0].data;
+          dataArr.map((data) => {
+            sum += Number(data);
+          });
+          let percentage = ((value * 100) / sum).toFixed(2) + "%";
+          return percentage;
+        },
+        color: "#fff",
+      },
+    },
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Pie type="pie" data={data} options={options} />
+      {/* <Pie type="pie" data={data2} options={options} />
+      <Pie type="pie" data={data3} options={options} /> */}
     </div>
   );
 }
 
 export default App;
+
+// const data1 = {
+//   labels: ["Yes", "No"],
+//   datasets: [
+//     {
+//       label: "My First dataset",
+//       data: [65, 59],
+//       backgroundColor: ["#4b77a9", "#5f255f"],
+//       hoverOffset: 4,
+//     },
+//   ],
+// };
+// const data2 = {
+//   labels: ["Yes", "No"],
+//   datasets: [
+//     {
+//       label: "My First dataset",
+//       data: [80, 56],
+//       backgroundColor: ["#5f255f", "#d21243"],
+//       hoverOffset: 4,
+//     },
+//   ],
+// };
+// const data3 = {
+//   labels: ["Yes", "No"],
+//   datasets: [
+//     {
+//       label: "My First dataset",
+//       data: [59, 106],
+//       backgroundColor: ["#d21243", "#B27200"],
+//       hoverOffset: 4,
+//       borderWidth: 2,
+//     },
+//   ],
+// };
---
+++ src/data.js
@@ -0,0 +1,65 @@
+export const NewData = [
+  {
+    label: "Hindi",
+    response: "12",
+  },
+  {
+    label: "Math",
+    response: "76",
+  },
+];
+
+export const NewData2 = [
+  [
+    {
+      label: "Hindi",
+      response: "12",
+    },
+    {
+      label: "Math",
+      response: "76",
+    },
+  ],
+  [
+    {
+      label: "English",
+      response: "82",
+    },
+    {
+      label: "Physics",
+      response: "76",
+    },
+  ],
+  [
+    {
+      label: "Yoruba",
+      response: "82",
+    },
+    {
+      label: "Chem",
+      response: "76",
+    },
+  ],
+];
+
+// export const Data = [
+//   { label: "Yes", response: 50 },
+//   { label: "No", response: 70 },
+// ];
+
+// export const NewData = [
+//   {
+//     pie1: [
+//       { label: "Yes", response: 50 },
+//       { label: "No", response: 70 },
+//     ],
+//   },
+//   {
+//     pie2: [
+//       {
+//         label: ["Yes", "No", "Never"],
+//         data: [50,18, 40]
+//       }
+//     ],
+//   },
+// ];
